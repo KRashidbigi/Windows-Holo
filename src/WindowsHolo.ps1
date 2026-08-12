c9b36c2c97021b6271f20721811d05938a65a80c
KRashidbigi <k.rashidbigi@outlook.com>
2026-08-12 22:11:57 +0100

    Update WindowsHolo.ps1

--- src/WindowsHolo.ps1
+++ src/WindowsHolo.ps1
@@ -2,7 +2,7 @@ $SampleRate = 44100
 $DurationMs = 40                                # FASTER: Shorter slice to minimize blind spots
 $BytesPerSample = 2                              
 $BufferSize = [int]($SampleRate * ($DurationMs / 1000) * $BytesPerSample)
-$RawVolumeThreshold = 3500                      
+$RawVolumeThreshold = 800                      
 $DoubleTapWindowMs = 400                        # SLIGHTLY WIDER: Gives a bit more room for the second hit
                     
 
